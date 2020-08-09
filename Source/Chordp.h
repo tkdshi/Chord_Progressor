368de6f6675e83d2165611ecfe38e81f49f4a9e1
tkdshi <62202033+tkdshi@users.noreply.github.com>
2020-08-10 00:15:31 +0900

    standaloneでピアノ音を鳴らす

--- Source/Chordp.h
+++ Source/Chordp.h
@@ -64,116 +64,7 @@ public:
     bool appliesToChannel (int /*midiChannel*/) override    { return true; }
 };
 
-//==============================================================================
-/** A simple demo synth voice that just plays a sine wave.. */
-class SineWaveVoice   : public SynthesiserVoice
-{
-public:
-    SineWaveVoice() {}
 
-    bool canPlaySound (SynthesiserSound* sound) override
-    {
-        return dynamic_cast<SineWaveSound*> (sound) != nullptr;
-    }
-
-    void startNote (int midiNoteNumber, float velocity,
-                    SynthesiserSound* /*sound*/,
-                    int /*currentPitchWheelPosition*/) override
-    {
-        currentAngle = 0.0;
-        level = velocity * 0.15;
-        tailOff = 0.0;
-
-        auto cyclesPerSecond = MidiMessage::getMidiNoteInHertz (midiNoteNumber);
-        auto cyclesPerSample = cyclesPerSecond / getSampleRate();
-
-        angleDelta = cyclesPerSample * MathConstants<double>::twoPi;
-    }
-
-    void stopNote (float /*velocity*/, bool allowTailOff) override
-    {
-        if (allowTailOff)
-        {
-            // start a tail-off by setting this flag. The render callback will pick up on
-            // this and do a fade out, calling clearCurrentNote() when it's finished.
-
-            if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
-                                // stopNote method could be called more than once.
-                tailOff = 1.0;
-        }
-        else
-        {
-            // we're being told to stop playing immediately, so reset everything..
-
-            clearCurrentNote();
-            angleDelta = 0.0;
-        }
-    }
-
-    void pitchWheelMoved (int /*newValue*/) override
-    {
-        // not implemented for the purposes of this demo!
-    }
-
-    void controllerMoved (int /*controllerNumber*/, int /*newValue*/) override
-    {
-        // not implemented for the purposes of this demo!
-    }
-
-    void renderNextBlock (AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
-    {
-        if (angleDelta != 0.0)
-        {
-            if (tailOff > 0.0)
-            {
-                while (--numSamples >= 0)
-                {
-                    auto currentSample = (float) (sin (currentAngle) * level * tailOff);
-
-                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample (i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-
-                    tailOff *= 0.99;
-
-                    if (tailOff <= 0.005)
-                    {
-                        // tells the synth that this voice has stopped
-                        clearCurrentNote();
-
-                        angleDelta = 0.0;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                while (--numSamples >= 0)
-                {
-                    auto currentSample = (float) (sin (currentAngle) * level);
-
-                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample (i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-                }
-            }
-        }
-    }
-
-    using SynthesiserVoice::renderNextBlock;
-
-private:
-    double currentAngle = 0.0;
-    double angleDelta   = 0.0;
-    double level        = 0.0;
-    double tailOff      = 0.0;
-
-
-};
 
 //==============================================================================
 /** As the name suggest, this class does the actual audio processing. */
@@ -190,8 +81,7 @@ public:
     {
         // Add a sub-tree to store the state of our UI
         state.state.addChild ({ "uiState", { { "width",  400 }, { "height", 200 } }, {} }, -1, nullptr);
-
-        initialiseSynth();
+        loadAudioFile();
     }
 
     ~JuceDemoPluginAudioProcessor() override = default;
@@ -580,6 +470,9 @@ private:
             lastUIWidth. addListener (this);
             lastUIHeight.addListener (this);
 
+
+
+
             updateTrackProperties();
 
             // start a timer which will keep our timecode display updated
@@ -953,17 +846,7 @@ private:
     CriticalSection trackPropertiesLock;
     TrackProperties trackProperties;
 
-    void initialiseSynth()
-    {
-        auto numVoices = 8;
 
-        // Add some voices...
-        for (auto i = 0; i < numVoices; ++i)
-            synth.addVoice (new SineWaveVoice());
-
-        // ..and give the synth a sound to play
-        synth.addSound (new SineWaveSound());
-    }
 
     void updateCurrentTimeInfoFromHost()
     {
